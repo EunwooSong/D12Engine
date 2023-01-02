1c2401e0d5b63023322b8a73fe90b9ff52f31c19
EunwooSong <song200348@gmail.com>
2023-01-03 00:50:43 +0900

    Imported DirectX headers

--- HIPR3D_Engine/HIPR3D_Engine/Common/d3dApp.cpp
+++ HIPR3D_Engine/HIPR3D_Engine/Common/d3dApp.cpp
@@ -414,26 +414,27 @@ bool D3DApp::InitMainWindow()
 
 bool D3DApp::InitDirect3D()
 {
-#if defined(DEBUG) || defined(_DEBUG) 
-	// Enable the D3D12 debug layer.
-{
-	ComPtr<ID3D12Debug> debugController;
-	ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)));
-	debugController->EnableDebugLayer();
-}
+#if defined(DEBUG) || defined(_DEBUG)
+	// D3D12 Debug Layer 활성화
+	{
+		ComPtr<ID3D12Debug> debugController;
+		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
+			debugController->EnableDebugLayer();
+		}
+	}
 #endif
 
 	ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&mdxgiFactory)));
 
-	// Try to create hardware device.
+	// HW Adapter를 나타내는 Device 생성
+	// D3D12CreateDevice: 
 	HRESULT hardwareResult = D3D12CreateDevice(
-		nullptr,             // default adapter
+		nullptr, 
 		D3D_FEATURE_LEVEL_11_0,
 		IID_PPV_ARGS(&md3dDevice));
 
-	// Fallback to WARP device.
-	if(FAILED(hardwareResult))
-	{
+	// 실패했다면 WARP Adapter를 나타내는 장치를 생성함.
+	if (FAILED(hardwareResult)) {
 		ComPtr<IDXGIAdapter> pWarpAdapter;
 		ThrowIfFailed(mdxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&pWarpAdapter)));
 
